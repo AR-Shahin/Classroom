8d7221fc6645bb99e2f1f26fd9f4ee85988f8b31
AR Shahin <mdshahinmije96@gmail.com>
2021-07-27 22:12:46 +0600

    29

---
+++ Arif/29/resources/js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $('.parent_div').slick({
+        slidesToShow : 4,
+        dots: false, 
+         fade:true,
+         arrows:false,
+         responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+      });
+});
